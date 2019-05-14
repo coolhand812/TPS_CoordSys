9410ccec2485ecf0087746dd910cb9244ca0d25c
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2019-05-14 08:44:42 -0500

    fixed format

--- grafica_barra.php
+++ grafica_barra.php
@@ -1,33 +1,33 @@
 <?php
- include('jpgraph.php');
- include('jpgraph_bar.php');
+    include('jpgraph.php');
+    include('jpgraph_bar.php');
 
-// Some dat
-$ydata = array(11.5,3,8,12,5,1,9,13,5,7);
+    // Some dat
+    $ydata = array(11.5,3,8,12,5,1,9,13,5,7);
 
-// Create the graph. These two calls are always required
-$graph = new Graph(450,250,"auto");
-$graph->SetScale("textlin");
-$graph->img->SetAntiAliasing();
-$graph->xgrid->Show();
+    // Create the graph. These two calls are always required
+    $graph = new Graph(450,250,"auto");
+    $graph->SetScale("textlin");
+    $graph->img->SetAntiAliasing();
+    $graph->xgrid->Show();
 
-// Create the linear plot
-$lineplot=new BarPlot($ydata);
-$lineplot->SetColor("black");
-$lineplot->SetWeight(2);
-$lineplot->SetLegend("Horas");
+    // Create the linear plot
+    $lineplot=new BarPlot($ydata);
+    $lineplot->SetColor("black");
+    $lineplot->SetWeight(2);
+    $lineplot->SetLegend("Horas");
 
-// Setup margin and titles
-$graph->img->SetMargin(40,20,20,40);
-$graph->title->Set("Ejemplo: Horas de Trabajo");
-$graph->xaxis->title->Set("Días");
-$graph->yaxis->title->Set("Horas de Trabajo");
-$graph->ygrid->SetFill(true,'#EFEFEF@0.5','#F9BB64@0.5');
-//$graph->SetShadow();
+    // Setup margin and titles
+    $graph->img->SetMargin(40,20,20,40);
+    $graph->title->Set("Ejemplo: Horas de Trabajo");
+    $graph->xaxis->title->Set("Días");
+    $graph->yaxis->title->Set("Horas de Trabajo");
+    $graph->ygrid->SetFill(true,'#EFEFEF@0.5','#F9BB64@0.5');
+    //$graph->SetShadow();
 
-// Add the plot to the graph
-$graph->Add($lineplot);
+    // Add the plot to the graph
+    $graph->Add($lineplot);
 
-// Display the graph
-$graph->Stroke();
+    // Display the graph
+    $graph->Stroke();
 ?> 
