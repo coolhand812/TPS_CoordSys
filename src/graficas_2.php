9d9f7239f8f35bb8018a96f891218371bbf289d8
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2019-05-20 22:05:50 -0500

    edited legend position, added labels, changed info

--- src/graficas_2.php
+++ src/graficas_2.php
@@ -2,19 +2,21 @@
     include ('jpgraph.php');
     include ('jpgraph_line.php');
 
-    $datay1 = array(20,15,23,15,80,20,45,10,5,45,60);
-    $datay2 = array(12,9,12,8,41,15,30,8,48,36,14,25);
-    $datay3 = array(5,17,32,24,4,2,36,2,9,24,21,23);
+    $datay1 = array(20,15,23,15,80,20,45,10,5,45,60);       //BAAP
+    $datay2 = array(12,10,12,11,15,17,15,17,19,21,18,16);   //BAM
+    $datay3 = array(3,3,3,3,3,3,1,1,1,1,1,1);               //BSAC
+    $datay4 = array(5,17,32,24,4,2,36,2,9,24,21,23);        //BSBA
 
     // Setup the graph
     $graph = new Graph(900,350);
     $graph->SetScale("textlin");
 
     $theme_class=new UniversalTheme;
-
+    
+    // Graph title
     $graph->SetTheme($theme_class);
     $graph->img->SetAntiAliasing(false);
-    $graph->title->Set('Evolución de pedidos');
+    $graph->title->Set('Students by Degree Program');
     $graph->SetBox(false);
 
     $graph->img->SetAntiAliasing();
@@ -25,28 +27,35 @@
 
     $graph->xgrid->Show();
     $graph->xgrid->SetLineStyle("solid");
-    $graph->xaxis->SetTickLabels(array('Ene','Feb','Mar','Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Nov', 'Oct', 'Dic'));
+    $graph->xaxis->SetTickLabels(array('Jan','Feb','Mar','Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'));
     $graph->xgrid->SetColor('#E3E3E3');
 
     // Create the first line
     $p1 = new LinePlot($datay1);
     $graph->Add($p1);
     $p1->SetColor("#6495ED");
-    $p1->SetLegend('Tienda 1');
+    $p1->SetLegend('B.A. Applied Psych.');
 
     // Create the second line
     $p2 = new LinePlot($datay2);
     $graph->Add($p2);
-    $p2->SetColor("#B22222");
-    $p2->SetLegend('Tienda 2');
+    $p2->SetColor("#58b02b");
+    $p2->SetLegend('B.A. in Management');
 
     // Create the third line
     $p3 = new LinePlot($datay3);
     $graph->Add($p3);
-    $p3->SetColor("#FF1493");
-    $p3->SetLegend('Tienda 3');
+    $p3->SetColor("#FF1111");
+    $p3->SetLegend('B.S. Applied Computing');
 
-    $graph->legend->SetFrameWeight(1);
+    // Create the fourth line
+    $p4 = new LinePlot($datay4);
+    $graph->Add($p4);
+    $p4->SetColor("#15edff");
+    $p4->SetLegend('B.S. in Business Admin.');
+
+    $graph->legend->SetFrameWeight(1);      // Sets value for box width
+    $graph->Legend->Pos(center, bottom);    // Sets legend position
 
     // Output line
     $graph->Stroke();
